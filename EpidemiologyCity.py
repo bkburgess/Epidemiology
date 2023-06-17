a73734f86a8a44a99e2b963aa6a40f8ef2895660
bkburgess <brycekburgess@gmail.com>
2023-06-16 23:49:47 -0700

    refactor for multiple diseases

--- EpidemiologyCity.py
+++ EpidemiologyCity.py
@@ -9,16 +9,16 @@ import logging
 
 class Disease():
     """
-    a disease that can infect people in a city
+    a disease that can infect people in a population
     """
-    def __init__(self, _inf_prob: float = 0.01, _death_prob: float = 0.01, _rec_prob: float = 0.01, _rein_prob: float = 0.001):
+    def __init__(self, _name: str = 'A', _inf_prob: float = 0.01, _death_prob: float = 0.01, _rec_prob: float = 0.01, _rein_prob: float = 0.001):
+        self.name           = _name
         self.infect_prob    = _inf_prob 
         self.death_prob     = _death_prob
         self.recover_prob   = _rec_prob
         self.reinfect_prob  = _rein_prob
         self.research_progress = 0
-    
-# one box refers to all the people in a city with a given status
+
 class Box():
     """
     A status or potential status for a person
@@ -28,8 +28,7 @@ class Box():
         self.n = n
         self.type = _type
 
-    # allows for the changing of people between different boxes
-    def transfer_people(self, n_change: int, box) -> int: # type hinting for Box doesn't seem to work here
+    def transfer_people(self, n_change: int, box: 'Box') -> int:
         """
         n_change: number of people to transfer
         box: a box to transfer people to
@@ -42,84 +41,88 @@ class Box():
         return n_change
 
 
-# one city refers to all people within a region of all statuses
-class City():
+class Population():
     """
     A large collection of people, mix of statuses, potentially infected by disease
     """
-    
-    def __init__(self, n_unf: int, n_inf: int, n_rec: int = 0, n_dead: int = 0, research_rate: float=random(), connections: list = []) -> None:
+    def __init__(self,  total: int, name: str) -> None:
+        self.total = total
         self.name = "Esterpool"
-        self.uninfected = Box(n_unf, _type='uninfected')
-        self.infected = Box(n_inf, _type='infected')
-        self.recovered = Box(n_rec, _type='recovered')
-        self.dead = Box(n_dead, _type='dead')
-        
-        self.research_rate = research_rate
+        self.diseases = {}
 
-        self.total = self.uninfected.n + self.infected.n + self.recovered.n + self.dead.n
-        self.con = connections
-        
+    def load_disease_info(self, disease_name: str, n_unf: int, n_inf: int, n_rec: int = 0, n_dead: int = 0, research_rate: float=0.001):
+        if n_unf + n_inf + n_rec + n_dead != self.total: 
+            raise ValueError("The total number of people must equal the population total")
+
+        self.diseases[disease_name] = {
+            'uninfected': Box(n_unf, _type='uninfected'),
+            'infected': Box(n_inf, _type='infected'),
+            'recovered': Box(n_rec, _type='recovered'),
+            'dead': Box(n_dead, _type='dead'),
+            'research_rate': research_rate
+        }
+    
     def __str__(self) -> str:
-        return "City: " + self.name
+        return "Population: " + self.name
 
-    def update_city_status(self, disease: Disease) -> None:
+    def update_population_status(self, disease: Disease) -> None:
         """
-        disease: disease that affects the city
+        disease: disease that affects the population
         """
-        self.infect(disease)
-        self.recover(disease)
-        self.die(disease)
+        self.infect(disease.infect_prob, disease.reinfect_prob)
+        self.recover(disease.recover_prob)
+        self.die(disease.death_prob)
         # self.research(disease)
         # self.impact = (self.dead.n + self.infected.n) / (self.dead.n + self.infected.n + self.uninfected.n + self.recovered.n)
+        assert self.uninfected.n + self.infected.n + self.dead.n + self.recovered.n == self.total
 
-    def infect(self, disease: Disease, prob_fn: str = "simple") -> None:
+    def infect(self, infect_prob: float, reinfect_prob: float, prob_fn: str = "simple") -> None:
         """
-        disease: a disease that can infect people in a city
+        disease: a disease that can infect people in a population
         prob_fn: how to calculate the rate of infection (ratio, erf)
         """
         # [ ] TODO include option to be dependent upon number dead
         # [ ] TODO adjust infected/reinfected per disease instead of total
         if prob_fn == 'simple':
-            new_infected = disease.infect_prob   * self.uninfected.n
-            reinfected   = disease.reinfect_prob * self.recovered.n
+            new_infected = infect_prob   * self.uninfected.n
+            reinfected   = reinfect_prob * self.recovered.n
         elif prob_fn == "ratio":
-            new_infected = disease.infect_prob   * self.uninfected.n * self.infected.n / (self.uninfected.n + self.infected.n)
-            reinfected   = disease.reinfect_prob * self.recovered.n  * self.infected.n / (self.recovered.n  + self.infected.n) 
+            new_infected = infect_prob   * self.uninfected.n * self.infected.n / (self.uninfected.n + self.infected.n)
+            reinfected   = reinfect_prob * self.recovered.n  * self.infected.n / (self.recovered.n  + self.infected.n) 
         elif prob_fn == "erf": 
-            new_infected = disease.infect_prob   * self.uninfected.n * erf(self.infected.n/self.uninfected.n)
-            reinfected   = disease.reinfect_prob * self.recovered.n  * erf(self.infected.n/self.recovered.n)
+            new_infected = infect_prob   * self.uninfected.n * erf(self.infected.n/self.uninfected.n)
+            reinfected   = reinfect_prob * self.recovered.n  * erf(self.infected.n/self.recovered.n)
 
         new_infected = round(new_infected)
         reinfected = round(reinfected)
         self.uninfected.transfer_people(new_infected, self.infected)
         self.recovered.transfer_people(reinfected, self.infected)
 
-    def recover(self, disease: Disease) -> int:
+    def recover(self, recover_prob: float) -> int:
         """
         disease: a disease that people will recover from
         """
-        new_recovered = disease.recover_prob * self.infected.n
+        new_recovered = recover_prob * self.infected.n
         new_recovered = round(new_recovered)
         return self.infected.transfer_people(new_recovered, self.recovered)
         
 
-    def die(self, disease: Disease) -> int:
+    def die(self, death_prob: float) -> int:
         """
         disease: a disease that people will die of
         """
-        died = disease.death_prob * self.infected.n
+        died = death_prob * self.infected.n
         died = round(died)
         return self.infected.transfer_people(died, self.dead)
 
-    def research(self, disease: Disease) -> int:
+    def research(self, research_progress: float) -> int:
         """
         disease: a disease that cities can research
         """
-        disease.research_progress += self.research_rate
-        return disease.research_progress
+        research_progress += self.research_rate
+        return research_progress
         
-    def print_info(self, disease: Disease) -> None:
+    def print_population_status(self, disease: Disease) -> None:
         print(f"uninfected: {self.uninfected.n}")
         print(f"infected: {self.infected.n}")
         print(f"recovered: {self.recovered.n}")
@@ -160,16 +163,17 @@ if __name__ == "__main__":
 
     init_uninfected = 1000
     init_infected = 10
-    disease = Disease()
-    city = City(init_uninfected, init_infected)
+    disease = Disease('andromeda')
+    pop = Population(init_infected + init_uninfected)
+    pop.load_disease_info(disease, init_uninfected, init_infected)
     record = {'uninfected': [init_uninfected], 'infected': [init_infected], 'dead': [], 'recovered': []}
     duration = 300
     for i in range(duration):
-        city.update_city_status(disease)
-        record['dead'].append(city.dead.n)
-        record['infected'].append(city.infected.n)
-        record['recovered'].append(city.recovered.n)
-        record['uninfected'].append(city.uninfected.n)
+        pop.update_population_status(disease)
+        record['dead'].append(pop.dead.n)
+        record['infected'].append(pop.infected.n)
+        record['recovered'].append(pop.recovered.n)
+        record['uninfected'].append(pop.uninfected.n)
 
     draw_plot(record)
     # make_graph()
